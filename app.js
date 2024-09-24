1d2387ef9b7f3e4b45d7e0058f28f01a40bb597e
kangaroo19 <1000jjj@naver.com>
2024-09-24 22:56:44 +0900

    puppeteer test

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
+import puppeteer from "puppeteer";
 
-const getHtml = async () => {
-  try {
-    // 1
-    const html = await axios.get("https://www.genie.co.kr/chart/top200");
-    let ulList = [];
-    // 2
-    const $ = cheerio.load(html.data);
-    // 3
-    const bodyList = $("tr.list");
-    bodyList.map((i, element) => {
-      ulList[i] = {
-        rank: i + 1,
-        // 4
-        title: $(element).find("td.info a.title").text().replace(/\s/g, ""),
-        artist: $(element).find("td.info a.artist").text().replace(/\s/g, ""),
-      };
-    });
-    console.log("bodyList : ", ulList);
-  } catch (error) {
-    console.error(error);
-  }
-};
+(async () => {
+  // 브라우저 실행
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  // 구글로 이동
+  await page.goto("https://www.google.com");
 
-getHtml();
+  // // 검색창에 검색어 입력 및 검색 실행
+  await page.type("input", "Puppeteer");
+  await page.keyboard.press("Enter");
+
+  // // 페이지가 로딩될 때까지 대기
+  await page.waitForSelector("h3");
+
+  // // 검색 결과의 제목을 가져오기
+  const titles = await page.evaluate(() => {
+    const elements = document.querySelectorAll("h3");
+    return Array.from(elements).map((el) => el.textContent);
+  });
+
+  // // 검색 결과 출력
+  console.log(titles);
+
+  // // 브라우저 종료
+  await browser.close();
+})();
